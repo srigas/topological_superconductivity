fdfc7f7629b422688bd9020712cc61dc5e89a036
Spyros <rigassp@gmail.com>
2023-11-08 15:43:41 +0200

    done for now with integrals docs, to be revisited

--- docs/3. Integrals/plotting.py
+++ docs/3. Integrals/plotting.py
@@ -17,3 +17,20 @@ def set_custom_grid():
     sns.set_style("darkgrid", {"axes.facecolor": "#3b3b3b", "grid.color": "#525252"})
 # ---------------------------------------------------------------------------------------
 
+def plot_contour(Es: np.ndarray):
+    # Setup the grid
+    set_custom_grid()
+
+    # Setup figure, set dimensions
+    plt.figure(figsize=(8, 4))
+
+    # Draw the points as a scatter plot, where x = Re(E) and y = Im(E)
+    Re_E, Im_E = Es.real, Es.imag
+    plt.scatter(Re_E, Im_E, s = 2.0, color='#d957ce')
+    plt.xlabel('Real Axis')
+    plt.ylabel('Imaginary Axis')
+    plt.title('Energy Mesh Plot')
+    
+    plt.show()
+
+    return
---
+++ tsc/impurity_atoms.py
@@ -0,0 +1,6 @@
+"""
+The impurity_atoms.py file holds functions that generate the required parameters for the problem's impurity atoms.
+The two main functions [1] create a single basis atom or [2] create a chain of magnetic impurities in a spin-helix
+configuration. Additional functions can be added to create different types of configurations, which may not be as universal.
+"""
+
